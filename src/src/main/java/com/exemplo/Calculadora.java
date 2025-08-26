f4adcb48ae8a0f117520a647f73e0b2337d52fbb
Emmylio <emmyliochaves07@gmail.com>
2025-08-25 21:47:19 -0300

    Adiciona classe Calculadora e Main com método somar

---
+++ src/src/main/java/com/exemplo/Calculadora.java
@@ -0,0 +1,10 @@
+package com.exemplo;
+
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    // Cada aluno pode implementar outro método depois
+}
---
+++ src/src/main/java/com/exemplo/Main.java
@@ -0,0 +1,8 @@
+package com.exemplo;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println("2 + 3 = " + calc.somar(2, 3));
+    }
+}
