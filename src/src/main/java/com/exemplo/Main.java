428c9bb092211be64e5fed2554c8a0244581a561
emmylio <emmyliochaves07@gmail.com>
2025-08-29 01:26:06 -0300

    Update Main.java - emmylio

--- src/src/main/java/com/exemplo/Main.java
+++ src/src/main/java/com/exemplo/Main.java
@@ -3,6 +3,9 @@ package com.exemplo;
 public class Main {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
-        System.out.println("2 + 3 = " + calc.somar(2, 3));
+        
+        System.out.println("Soma: = " + calc.somar(2, 3));
+        System.out.println("Multiplicação: " + calc.multiplicar(4, 2));
+        System.out.println("Divisão: " + calc.dividir(10, 2));
     }
 }
