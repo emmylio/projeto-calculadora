423c1c1fb630c25427a696220181587b53be7f92
Ytallo7 <salesytallo4@gmail.com>
2025-09-01 23:14:24 -0300

    Implementa método subtrair na Calculadora

---
+++ subtrair.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+
+    // Método de subtração
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    // (outros métodos como somar, dividir, multiplicar, etc.)
+}
